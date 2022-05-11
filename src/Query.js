c125d0bd0c1c8b0ed1aa6de5a2f093ffe9d9d8d2
Shane Patrick Roach <roach.patrick.shane@gmail.com>
2022-05-11 09:56:26 -0700

    initial commit with basic query to graaphql

---
+++ src/Query.js
@@ -0,0 +1,20 @@
+const githubQuery = {
+  query: `
+  {
+    viewer {
+      name
+      repositories(first: 10) {
+        nodes {
+          name
+          description
+          id
+          url
+        }
+      }
+    }
+  }
+  `
+  ,
+};
+
+export default githubQuery;
